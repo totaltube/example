2abc159566c9db98f45213834133f0ebd50f7ff8
totaltube <admin@totaltraffictrader.com>
2022-11-23 16:41:51 +0300

    some cleanup

--- sites/commonsite.dev/extensions/function-ad_url.js
+++
@@ -1,9 +0,0 @@
-// Example function which outputs advertising url depending on zone name
-
-function ad_url(zone, pos) {
-    var ret = "https://aa.sersh.com/ds/" + zone + "?d=" + encodeURIComponent(host)
-    if (pos) {
-        ret += "&pos=" + pos.toString()
-    }
-    return ret
-}
